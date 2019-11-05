d8894f1ec48ac25acda8de7314bdc949bbbeb1ae
rubengrillaert <ruben.grillaert@hotmail.com>
2019-11-05 20:06:20 +0100

    Login page and passenger model

---
+++ OnBoardFlight/OnBoardFlight/Model/Passenger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoardFlight.Model
+{
+    class Passenger : INotifyPropertyChanged
+    {
+        private int seatNumber;
+
+        public int SeatNumber
+        {
+            get { return seatNumber; }
+            set { seatNumber = value; RaisePropertyChanged("SeatNumber"); }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; RaisePropertyChanged("Password"); }
+        }
+
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
