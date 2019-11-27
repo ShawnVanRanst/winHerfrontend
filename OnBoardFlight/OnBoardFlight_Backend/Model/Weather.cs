08d8c1c7fd02a4cb630dd9ce6290a5d059bcedcb
ArnoBoel <arno-boel@hotmail.be>
2019-11-27 17:06:54 +0100

    Add weather model class

---
+++ OnBoardFlight/OnBoardFlight_Backend/Model/Weather.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnBoardFlight_Backend.Model
+{
+    public class Weather
+    {
+
+        #region Properties
+        public int WeatherId { get; set; }
+
+        public double Temp { get; set; }
+
+        public string Description { get; set; }
+
+        public double WindSpeed { get; set; }
+
+        public double WindOrientation { get; set; }
+
+        public string When { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Weather()
+        {
+
+        }
+
+        public Weather(double temp, string description, double windSpeed, double windOrientation, string when)
+        {
+            Temp = temp;
+            Description = description;
+            WindSpeed = windSpeed;
+            WindOrientation = windOrientation;
+            When = when;
+        }
+
+
+        #endregion
+    }
+}
