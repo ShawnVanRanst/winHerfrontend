fa73e4b5159f71e3d969f263b184ba76760ba3a8
ArnoBoel <arno-boel@hotmail.be>
2019-11-27 17:02:34 +0100

    Add Chat and Message model classes

---
+++ OnBoardFlight/OnBoardFlight_Backend/Model/Chat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnBoardFlight_Backend.Model
+{
+    public class Chat
+    {
+
+        public int ChatId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Passenger> Participants { get; set; }
+
+        public ICollection<Message> Messages{ get; set; }
+
+
+        #region Constructors
+        public Chat()
+        {
+            Participants = new List<Passenger>();
+            Messages = new List<Message>();
+        }
+
+        public Chat(string name)
+        {
+            Name = name;
+        }
+        #endregion
+
+        #region Methods
+
+        public void AddParticipants(Passenger passenger)
+        {
+            Participants.Add(passenger);
+        }
+
+        public void AddMessage(Message message)
+        {
+            Messages.Add(message);
+        }
+        #endregion
+    }
+}
---
+++ OnBoardFlight/OnBoardFlight_Backend/Model/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnBoardFlight_Backend.Model
+{
+    public class Message
+    {
+
+        #region Properties
+
+        public int MessageId { get; set; }
+
+        public DateTime SendDate { get; set; }
+
+        public Passenger Sender { get; set; }
+
+
+        #endregion
+
+        #region Constructors
+
+        public Message()
+        {
+
+        }
+
+        public Message(DateTime sendDate, Passenger sender)
+        {
+            SendDate = sendDate;
+            Sender = sender;
+        }
+
+
+        #endregion
+    }
+}
