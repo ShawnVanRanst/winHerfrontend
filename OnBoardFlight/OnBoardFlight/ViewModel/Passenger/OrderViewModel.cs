7b529464be6a6257adff8aa4a6b7de8b557242ff
ArnoBoel <arno-boel@hotmail.be>
2019-12-10 18:36:22 +0100

    Add view MyOrders

--- OnBoardFlight/OnBoardFlight/ViewModel/Passenger/OrderViewModel.cs
+++ OnBoardFlight/OnBoardFlight/ViewModel/Passenger/OrderViewModel.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnBoardFlight.ViewModel.Passenger
 {
-    public class OrderViewModel
+    public class OrderViewModel: INotifyPropertyChanged
     {
+
+        private Model.Order _order;
+
+        public Model.Order Order
+        {
+            get { return _order; }
+            set
+            {
+                _order = value;
+                RaisePropertyChanged("Order");
+            }
+        }
+
+
+        public OrderViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
