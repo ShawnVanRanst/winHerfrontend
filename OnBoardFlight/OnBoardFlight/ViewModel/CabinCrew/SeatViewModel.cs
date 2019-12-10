accff35ec62a18d8b932e728d6536f0e3fa9ac5d
ArnoBoel <arno-boel@hotmail.be>
2019-12-10 12:43:56 +0100

    Add SearchPassengerCommand

---
+++ OnBoardFlight/OnBoardFlight/ViewModel/CabinCrew/SeatViewModel.cs
@@ -0,0 +1,37 @@
+﻿using OnBoardFlight.ViewModel.Commands.CabinCrew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoardFlight.ViewModel.CabinCrew
+{
+    public class SeatViewModel
+    {
+
+
+        public Model.Passenger Passenger1 { get; set; }
+
+        public Model.Passenger Passenger2 { get; set; }
+
+        public string SeatPassenger1 { get; set; }
+
+        public string SeatPassenger2 { get; set; }
+
+
+        public SearchPassengerCommand SearchPassengerCommand{ get; set; }
+
+
+        public SeatViewModel()
+        {
+            SearchPassengerCommand = new SearchPassengerCommand(this);
+        }
+
+
+        public void SearchPassenger()
+        {
+
+        }
+    }
+}
