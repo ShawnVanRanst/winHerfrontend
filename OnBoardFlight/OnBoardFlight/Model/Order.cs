d1c8dababa9fec897afb13495948b1e08398e693
ArnoBoel <arno-boel@hotmail.be>
2019-12-10 17:38:09 +0100

    Add Order model class

--- OnBoardFlight/OnBoardFlight/Model/Order.cs
+++ OnBoardFlight/OnBoardFlight/Model/Order.cs
@@ -8,5 +8,19 @@ namespace OnBoardFlight.Model
 {
     public class Order
     {
+
+        public Passenger Passenger { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public ICollection<Orderline> Orderlines { get; set; }
+
+
+        public void AddOrderline(Orderline orderline)
+        {
+            Orderlines.Add(orderline);
+        }
     }
 }
